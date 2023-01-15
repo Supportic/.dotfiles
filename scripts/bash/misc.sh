69988764f3d414b141de363cc61258d5d9755903
Alexander Senger <senger.alexander@yahoo.de>
2023-01-15 16:52:50 +0100

    update install path NVM_DIR, add install_packages util

--- scripts/bash/misc.sh
+++ scripts/bash/misc.sh
@@ -19,3 +19,14 @@ function sudoIf() { if [ "$(id -u)" -ne 0 ]; then sudo "$@"; else "$@"; fi }
 function isTrue() {
   if [[ "${@^^}" =~ ^(TRUE$|YES$|Y$|ON$|1$) ]]; then return 0; fi; return 1
 }
+
+# installs apt packages if doesn't exist (root user required)
+# usage: install_packages curl ca-certificates
+function install_packages() {
+  if ! dpkg -s "$@" > /dev/null 2>&1; then
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+      apt-get update -y
+    fi
+    apt-get -y install --no-install-recommends "$@"
+  fi
+}
